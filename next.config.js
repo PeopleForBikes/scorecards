1ff9a907fb1254715ad0021674f113a4bf85d12d
Rémy Greinhofer <remy.greinhofer@gmail.com>
2022-11-05 08:44:31 -0500

    Fix static export (#5)
    Fixes the static export configuration.
    
    Drive-by:
    - Fixes the invalid SVG attributes.
    
    Signed-off-by: Rémy Greinhofer <remy.greinhofer@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+const isProduction = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
+  basePath: isProduction ? "/scorecard-portal" : "",
+  assetPrefix: isProduction ? "/scorecard-portal" : "",
 };
 
 module.exports = nextConfig;
