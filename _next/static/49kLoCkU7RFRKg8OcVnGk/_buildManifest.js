9a7827d3d3de83da8e0a5d8594e0d57d89a3bf49
rgreinho <rgreinho@users.noreply.github.com>
2023-02-15 16:11:26 +0000

    deploy: 0c072d264b0587746f04b4fd0aa9e4434989760a

---
+++ _next/static/49kLoCkU7RFRKg8OcVnGk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ccea4988e3f9ab04.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
