61850ab88a0ae7c3d739514ed4d734bacc38358a
Rémy Greinhofer <remy.greinhofer@gmail.com>
2023-04-04 13:39:21 -0500

    Fix Flag API. (#12)
    The current flag API stopped working, therefore we decided to swith to
    another one.
    
    Signed-off-by: Rémy Greinhofer <remy.greinhofer@gmail.com>

--- components/card/card.js
+++ components/card/card.js
@@ -3,17 +3,19 @@ import Image from "next/image";
 export default function Card(props) {
   return (
     <div className="p-2 lg:w-1/4 md:w-1/2 w-full">
-      <a href={props.base_url + props.country.replace(" ", "_") + ".zip"}>
+      <a
+        href={props.base_url + props.country.country.replace(" ", "_") + ".zip"}
+      >
         <div className="flex items-center border-gray-900 border p-4 rounded-lg">
           <img
             alt={props.country}
             className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4 border"
             crossorigin="anonymous"
-            src={"https://countryflagsapi.com/svg/" + props.country}
+            src={"https://flagcdn.com/" + props.country.code + ".svg"}
           />
           <div className="flex-grow">
             <h2 className="text-gray-900 title-font font-medium capitalize">
-              {props.country}
+              {props.country.country}
             </h2>
           </div>
         </div>
